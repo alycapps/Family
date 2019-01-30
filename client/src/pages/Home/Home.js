a561f67ef4d6d21f83ccdf4a87d30d7655d3f3a7
Aly <38168385+alycapps@users.noreply.github.com>
2019-01-30 18:53:27 -0500

    initial file setup

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {
+    
+  };
+
+  componentDidMount() {
+  };
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Home;
